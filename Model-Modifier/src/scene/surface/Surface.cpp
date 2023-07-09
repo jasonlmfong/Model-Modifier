93799876a8bbee8d9c430d70f1996676b53354ab
Jason Fong <jasonlmfong@gmail.com>
2023-07-09 15:52:24 -0400

    added surface class to parse topological information of objects

---
+++ Model-Modifier/src/scene/surface/Surface.cpp
@@ -0,0 +1,80 @@
+﻿#include "Surface.h"
+
+unsigned int Surface::getEdgeIndex(glm::uvec2 vertPair)
+{
+    glm::uvec2 orderedVertPair = vertPair;
+    if (vertPair.x > vertPair.y) // check if the order is maintained (idx1 < idx2)
+    {
+        // if it is violated, swap it
+        orderedVertPair.x = vertPair.y;
+        orderedVertPair.y = vertPair.x;
+    }
+
+    // search if starting vertex in our lookup
+    auto searchx = m_EdgeIdxLookup.find(orderedVertPair.x);
+    if (searchx != m_EdgeIdxLookup.end())
+    {
+        // search if ending vertex in our lookup
+        auto searchy = searchx->second.find(orderedVertPair.y);
+        if (searchy != searchx->second.end())
+        {
+            // found, return edge index
+            return searchy->second;
+        }
+    }
+    // not found, create new edge and insert at the end of m_Edges
+    unsigned int newEdgeIdx = m_Edges.size();
+    EdgeRecord newEdge;
+    newEdge.endPoint1Idx = orderedVertPair.x;
+    newEdge.endPoint2Idx = orderedVertPair.y;
+    m_Edges.push_back(newEdge);
+    m_EdgeIdxLookup[orderedVertPair.x].insert(std::make_pair(orderedVertPair.y, newEdgeIdx));
+    return newEdgeIdx;
+}
+
+Surface::Surface(Object obj)
+{
+    for (glm::vec3 vertPos : obj.m_VertexPos)
+    {
+        // populate m_Vertices with obj.m_VertexPos data
+        VertexRecord newVertex;
+        newVertex.position = vertPos;
+        m_Vertices.push_back(newVertex);
+    }
+
+    for (std::vector<unsigned int> faceVertices : obj.m_FaceIndices)
+    {
+        // create face record to be stored
+        FaceRecord newFace;
+        newFace.verticesIdx = faceVertices;
+
+        // get edges next to the current face
+        for (int i = 0; i < 3; i++)
+        {
+            unsigned int startVertex = faceVertices[i];
+            unsigned int endVertex = faceVertices[(i + 1) % 3];
+            unsigned int edgeIndex = getEdgeIndex({ startVertex, endVertex });
+
+            newFace.edgesIdx.push_back(edgeIndex);
+
+            // add the edges to the connecting vertices
+            m_Vertices[startVertex].adjEdgesIdx.push_back(edgeIndex);
+            m_Vertices[endVertex].adjEdgesIdx.push_back(edgeIndex);
+        }
+
+        unsigned int faceIndex = m_Faces.size();
+        m_Faces.push_back(newFace);
+
+        // add face index to the connecting vertices and edges 
+        for (int i = 0; i < 3; i++)
+        {
+            m_Vertices[faceVertices[i]].adjFacesIdx.push_back(faceIndex);
+            m_Edges[newFace.edgesIdx[i]].adjFacesIdx.push_back(faceIndex);
+        }
+    }
+}
+
+Surface::~Surface()
+{
+}
+
---
+++ Model-Modifier/src/scene/surface/Surface.h
@@ -0,0 +1,49 @@
+#include <vector>
+#include <unordered_map>
+#include <map>
+
+#include "../../external/glm/ext/vector_float3.hpp"
+#include "../../external/glm/ext/vector_uint2.hpp"
+#include "../../external/glm/geometric.hpp"
+
+#include "../Object.h"
+
+struct VertexRecord
+{
+	glm::vec3 position;
+	std::vector<unsigned int> adjEdgesIdx;
+	std::vector<unsigned int> adjFacesIdx;
+};
+
+struct EdgeRecord
+{
+	unsigned endPoint1Idx; // maintain that endpoint1 < endpoint2 for searching
+	unsigned endPoint2Idx;
+	std::vector<unsigned int> adjFacesIdx; // should have 1 or 2 adjacent faces
+};
+
+struct FaceRecord
+{
+	std::vector<unsigned int> verticesIdx; // each face can have 3 vertices
+	std::vector<unsigned int> edgesIdx; // each face can have 3 edges
+};
+
+class Surface
+{
+public:
+	// given the pair of indices in m_Vertices, find index in m_Edges
+	// if it does not exist, insert into our map and return
+	unsigned int getEdgeIndex(glm::uvec2 vertPair);
+
+	Surface(Object obj);
+	~Surface();
+
+	Object CatmullClark();
+
+public:
+	std::vector<VertexRecord> m_Vertices;
+	std::vector<EdgeRecord> m_Edges;
+	std::vector<FaceRecord> m_Faces;
+
+	std::unordered_map<unsigned int, std::unordered_map<unsigned int, unsigned int>> m_EdgeIdxLookup;
+};
