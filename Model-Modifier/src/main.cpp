670d475752d3fe94e5532afacbe63412b167b181
Jason Fong <jasonlmfong@gmail.com>
2024-05-04 16:04:17 -0400

    refactor screenshot function

--- Model-Modifier/src/main.cpp
+++ Model-Modifier/src/main.cpp
@@ -16,6 +16,7 @@
 #include "renderer/IndexBuffer.h"
 #include "renderer/Shader.h"
 #include "renderer/Camera.h"
+#include "renderer/SaveImage.h"
 #include "scene/object/Object.h"
 #include "scene/object/Objects.h"
 #include "scene/Mesh.h"
@@ -40,21 +41,6 @@ enum renderMode
     WIREFRAME,
     POINTCLOUD
 };
-
-void saveImage(const std::string &filepath, GLFWwindow *window) 
-{
-    int width, height;
-    glfwGetFramebufferSize(window, &width, &height);
-    GLsizei stride = 3 * width;
-    stride += (stride % 4) ? (4 - stride % 4) : 0;
-    GLsizei bufferSize = stride * height;
-    std::vector<char> buffer(bufferSize);
-    glPixelStorei(GL_PACK_ALIGNMENT, 4);
-    glReadBuffer(GL_FRONT);
-    glReadPixels(0, 0, width, height, GL_RGB, GL_UNSIGNED_BYTE, buffer.data());
-    stbi_flip_vertically_on_write(true);
-    stbi_write_png(filepath.c_str(), width, height, 3, buffer.data(), stride);
-}
 
 int main()
 {
