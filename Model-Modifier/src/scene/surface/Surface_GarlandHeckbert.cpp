fd3e984d8ae1c9a44df769bbc2e6d22ad94ba679
jasonlmfong <jasonlmfong@gmail.com>
2025-11-22 23:30:17 -0500

    fix bug in solver matrix for GH QEM

--- Model-Modifier/src/scene/surface/Surface_GarlandHeckbert.cpp
+++ Model-Modifier/src/scene/surface/Surface_GarlandHeckbert.cpp
@@ -74,9 +74,9 @@ glm::mat4 Surface::ComputeQuadric(VertexRecord v0)
 glm::mat4 Surface::BuildQuadricSolverMatrix(const glm::mat4& Quad)
 {
     glm::mat4 MatQ;
-    MatQ[0][0] = Quad[0][0]; MatQ[1][0] = Quad[1][0]; MatQ[2][0] = Quad[2][0]; MatQ[3][0] = Quad[3][0];
-    MatQ[0][1] = Quad[0][1]; MatQ[1][1] = Quad[1][1]; MatQ[2][1] = Quad[2][1]; MatQ[3][1] = Quad[3][1];
-    MatQ[0][2] = Quad[0][2]; MatQ[1][2] = Quad[1][2]; MatQ[2][2] = Quad[2][2]; MatQ[3][2] = Quad[3][2];
+    MatQ[0][0] = Quad[0][0]; MatQ[1][0] = Quad[0][1]; MatQ[2][0] = Quad[0][2]; MatQ[3][0] = Quad[0][3];
+    MatQ[0][1] = Quad[0][1]; MatQ[1][1] = Quad[1][1]; MatQ[2][1] = Quad[1][2]; MatQ[3][1] = Quad[1][3];
+    MatQ[0][2] = Quad[0][2]; MatQ[1][2] = Quad[1][2]; MatQ[2][2] = Quad[2][2]; MatQ[3][2] = Quad[2][3];
     MatQ[0][3] = 0;          MatQ[1][3] = 0;          MatQ[2][3] = 0;          MatQ[3][3] = 1;
     return MatQ;
 }
